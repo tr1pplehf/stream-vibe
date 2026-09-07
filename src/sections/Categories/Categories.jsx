578628dcccd65cac84dc3592e358188b6a899315
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-08 06:45:18 +0700

    #21 Stream-Vibe: -Секция Categories — компоненты CategoryCard и Image.

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,8 +1,56 @@
 import './Categories.scss'
 import Section from '@/layouts/Sections'
+import CategoryCard from '@/components/CategoryCard'
 
 const Categories = (props) => {
   const {} = props
+  const categoryItems = [
+    {
+      title: 'Action',
+      images: [
+        '/src/assets/images/categories/action/1.jpg',
+        '/src/assets/images/categories/action/2.jpg',
+        '/src/assets/images/categories/action/3.jpg',
+        '/src/assets/images/categories/action/4.jpg',
+      ]
+    },
+    {
+      title: 'Adventure',
+      images: [
+        '/src/assets/images/categories/adventure/1.jpg',
+        '/src/assets/images/categories/adventure/2.jpg',
+        '/src/assets/images/categories/adventure/3.jpg',
+        '/src/assets/images/categories/adventure/4.jpg',
+      ]
+    },
+    {
+      title: 'Comedy',
+      images: [
+        '/src/assets/images/categories/comedy/1.jpg',
+        '/src/assets/images/categories/comedy/2.jpg',
+        '/src/assets/images/categories/comedy/3.jpg',
+        '/src/assets/images/categories/comedy/4.jpg',
+      ]
+    },
+    {
+      title: 'Drama',
+      images: [
+        '/src/assets/images/categories/drama/1.jpg',
+        '/src/assets/images/categories/drama/2.jpg',
+        '/src/assets/images/categories/drama/3.jpg',
+        '/src/assets/images/categories/drama/4.jpg',
+      ]
+    },
+    {
+      title: 'Horror',
+      images: [
+        '/src/assets/images/categories/horror/1.jpg',
+        '/src/assets/images/categories/horror/2.jpg',
+        '/src/assets/images/categories/horror/3.jpg',
+        '/src/assets/images/categories/horror/4.jpg',
+      ]
+    },
+  ]
 
   return (
     <Section
@@ -17,7 +65,9 @@ const Categories = (props) => {
       )}
       isActionsHiddenOnMobile
     >
-
+      {categoryItems.map((categoryItem, index) => (
+        <CategoryCard {...categoryItem} key={index} />
+      ))}
     </Section>
   )
 }
