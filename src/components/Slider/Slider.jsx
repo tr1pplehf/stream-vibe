c91854f2c8c993b603a23b4d47cd4812a2bab3e5
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-08 08:35:34 +0700

    #24 Stream-Vibe: -Секция Categories — JavaScript-модуль SliderCollection. (part 2)

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,37 @@
 import 'swiper/css';
 import './Slider.scss'
 import SliderNavigation from './components/SliderNavigation'
+import classNames from 'classnames'
 
 const defaultSliderParams = {
   slidesPerView: 5,
   slidesPerGroup: 5,
   spaceBetween: 30,
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 20,
+    },
+    481: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      spaceBetween: 20,
+      allowTouchMove: false,
+    },
+    1441: {
+      spaceBetween: 30,
+      allowTouchMove: false,
+    }
+  }
 }
 
 const Slider = (props) => {
@@ -13,11 +39,15 @@ const Slider = (props) => {
     children,
     navigationTargetElementId = null,
     sliderParams = defaultSliderParams,
+    isBeyondTheViewportOnMobileS,
+    hasScrollbar = true,
   } = props
 
   return (
     <div
-      className="slider"
+      className={classNames("slider", {
+        'slider--beyond-the-viewport-on-mobile-s': isBeyondTheViewportOnMobileS
+      })}
       data-js-slider={JSON.stringify({
         sliderParams,
         navigationTargetElementId,
@@ -35,6 +65,12 @@ const Slider = (props) => {
       {!navigationTargetElementId && (
         <SliderNavigation
           className="slider__navigation"
+        />
+      )}
+      {hasScrollbar && (
+        <div
+          className="slider__scrollbar visible-mobile"
+          data-js-slider-scrollbar=""
         />
       )}
     </div>
--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -23,6 +23,7 @@ const Categories = () => {
     >
       <Slider
         navigationTargetElementId={sliderNavigationId}
+        isBeyondTheViewportOnMobileS
       >
         {categoryItems.map((categoryItem, index) => (
           <CategoryCard {...categoryItem} key={index} />
