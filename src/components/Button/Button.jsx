bc3a49f4a0f1dcc80443ff9ca14210939971b021
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-08 08:13:44 +0700

    #23 Stream-Vibe: -Секция Categories — JavaScript-модуль SliderCollection.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -20,6 +20,7 @@ const Button = (props) => {
      */
     iconPosition = 'before',
     hasFillIcon,
+    extraAttrs,
   } = props
   const isLink = href !== undefined
   const Component = isLink ? 'a' : 'button'
@@ -43,6 +44,7 @@ const Button = (props) => {
       title={title}
       aria-label={title}
       {...specificProps}
+      {...extraAttrs}
     >
       {iconPosition === 'before' && iconComponent}
       {!isLabelHidden && (
