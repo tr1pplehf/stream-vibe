2f83de1f714af627e27d0a862198fdbbe03c97f0
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-10 05:12:07 +0700

    #54 Stream-Vibe: -Секция Support — JavaScript-модуль InputMask.

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -14,10 +14,17 @@ const Field = (props) => {
     placeholder,
     isRequired,
     inputMode,
+    mask,
   } = props
   const Component = type === 'textarea'
     ? 'textarea'
     : 'input'
+
+  const extraAttrs = {}
+
+  if (mask) {
+    extraAttrs['data-js-input-mask'] = mask
+  }
 
   return (
     <div className={classNames(className, 'field')}>
@@ -42,6 +49,7 @@ const Field = (props) => {
           placeholder={placeholder}
           required={isRequired}
           inputMode={inputMode}
+          {...extraAttrs}
         />
       </div>
     </div>
--- src/sections/Support/Support.jsx
+++ src/sections/Support/Support.jsx
@@ -54,6 +54,7 @@ const Support = (props) => {
           label="Phone Number"
           placeholder="(999) 999-99-99"
           inputMode="tel"
+          mask="(000) 000-00-00"
         />
         <Field
           className="support__form-cell support__form-cell--wide"
