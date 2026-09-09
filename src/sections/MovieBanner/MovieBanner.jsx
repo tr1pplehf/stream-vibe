0930bd351a79cc1f8c788963d7e01e17295a96a4
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-09 11:18:30 +0700

    #41 Stream-Vibe: -Секция MovieBanner, доработка компонента MovieBannerCard

---
+++ src/sections/MovieBanner/MovieBanner.jsx
@@ -0,0 +1,25 @@
+import './MovieBanner.scss'
+import MovieBannerCard from '@/components/MovieBannerCard'
+
+const MovieBanner = (props) => {
+  const {} = props
+  const titleId = 'movie-banner-title'
+
+  return (
+    <section
+      className="container"
+      aria-labelledby={titleId}
+    >
+      <MovieBannerCard
+        title="Kantara"
+        titleId={titleId}
+        TitleTag="h1"
+        description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
+        imgSrc="/src/assets/images/movie-banner/2.jpg"
+        isSmallPaddingY
+      />
+    </section>
+  )
+}
+
+export default MovieBanner  
