daaa9d1c3e23bf6c56be1bf038552f70f250249f
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-09 12:24:43 +0700

    #44 Stream-Vibe: -Секция MovieDetails — компоненты Tags, Rating и PersonCard.

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -5,7 +5,11 @@ const PersonCard = (props) => {
   const {
     imgSrc,
     imgAlt,
+    name,
+    subtitle,
   } = props
+
+  const hasBody = Boolean(name || subtitle)
 
   return (
     <div className="person-card">
@@ -15,6 +19,20 @@ const PersonCard = (props) => {
         alt={imgAlt}
         title={imgAlt}
       />
+      {hasBody && (
+        <div className="person-card__body">
+          {name && (
+            <h4 className="person-card__name">
+              {name}
+            </h4>
+          )}
+          {subtitle && (
+            <p className="person-card__subtitle">
+              {subtitle}
+            </p>
+          )}
+        </div>
+      )}
     </div>
   )
 }
--- src/sections/MovieDetails/MovieDetails.jsx
+++ src/sections/MovieDetails/MovieDetails.jsx
@@ -4,12 +4,13 @@ import Slider from '@/components/Slider'
 import PersonCard from '@/components/PersonCard'
 import Button from '@/components/Button'
 import ReviewCard from '@/components/ReviewCard'
+import Icon from '@/components/Icon'
+import Tags from '@/components/Tags'
+import Ratings from '@/components/Ratings'
 
-const MovieDetails = (props) => {
-  const {} = props
+const MovieDetails = () => {
   const titleId = 'movie-details-title'
   const castSliderNavigationId = 'movie-card-slider-navigation'
-
   const castItems = [
     {
       imgSrc: '/src/assets/images/cast/1.jpg',
@@ -51,7 +52,6 @@ const MovieDetails = (props) => {
       imgAlt: 'Ivan Ivanov'
     },
   ]
-
   const reviewItems = [
     {
       name: 'Aniket Roy',
@@ -110,6 +110,24 @@ const MovieDetails = (props) => {
     },
 
   ]
+  const tagsItems = [
+    'English',
+    'Hindi',
+    'Tamil',
+    'Telegu',
+    'Kannada',
+  ]
+  const ratingItems = [
+    {
+      title: 'IMDb',
+      ratingValue: 4.5,
+    },
+    {
+      title: 'StreamVibe',
+      ratingValue: 4,
+    },
+  ]
+  const genresItems = ['Action', 'Adventure']
 
   return (
     <section
@@ -201,7 +219,54 @@ const MovieDetails = (props) => {
       </div>
       <aside className="movie-details__info">
         <div className="movie-details__panel">
-
+          <div className="movie-details__groups">
+            <div className="movie-details__group">
+              <h3 className="movie-details__title">
+                <Icon src="/src/assets/sprite/calendar.svg" />
+                <span>Released Year</span>
+              </h3>
+              <div className="movie-details__description">
+                <time className="h6" dateTime="2022">2022</time>
+              </div>
+            </div>
+            <div className="movie-details__group">
+              <h3 className="movie-details__title">
+                <Icon src="/src/assets/sprite/language.svg" />
+                <span>Available Languages</span>
+              </h3>
+              <Tags items={tagsItems} />
+            </div>
+            <div className="movie-details__group">
+              <h3 className="movie-details__title">
+                <Icon src="/src/assets/sprite/star.svg" />
+                <span>Ratings</span>
+              </h3>
+              <Ratings items={ratingItems} />
+            </div>
+            <div className="movie-details__group">
+              <h3 className="movie-details__title">
+                <Icon src="/src/assets/sprite/genres.svg" />
+                <span>Genres</span>
+              </h3>
+              <Tags items={genresItems} />
+            </div>
+            <div className="movie-details__group">
+              <h3 className="movie-details__title">Director</h3>
+              <PersonCard
+                name="Rishab Shetty"
+                subtitle="From India"
+                imgSrc="/src/assets/images/cast/director.jpg"
+              />
+            </div>
+            <div className="movie-details__group">
+              <h3 className="movie-details__title">Music</h3>
+              <PersonCard
+                name="B. Ajaneesh Loknath"
+                subtitle="From India"
+                imgSrc="/src/assets/images/cast/music.jpg"
+              />
+            </div>
+          </div>
         </div>
       </aside>
     </section>
