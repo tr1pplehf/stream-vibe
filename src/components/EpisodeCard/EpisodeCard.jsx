d0a532db73b9b2b76c8ec7132d141b376b577b12
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-09 16:02:54 +0700

    #51 Stream-Vibe: -Секция MovieDetails, JavaScript-модуль VideoPlayer

--- src/components/EpisodeCard/EpisodeCard.jsx
+++ src/components/EpisodeCard/EpisodeCard.jsx
@@ -17,18 +17,24 @@ const EpisodeCard = (props) => {
       <div className="episode-card__number">
         {number}
       </div>
-      <div className="episode-card__player">
+      <div
+        className="episode-card__player"
+        data-js-video-player=""
+      >
         <video
           className="episode-card__video"
           src={video.src}
           poster={video.poster}
           width="172"
           height="118"
+          data-js-video-player-video=""
         />
         <button
           className="episode-card__play-button is-active"
+          type="button"
           aria-label={playButtonTitle}
           title={playButtonTitle}
+          data-js-video-player-play-button=""
         >
           <Icon
             className="episode-card__play-button-icon"
