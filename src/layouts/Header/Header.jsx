417f0c610ca250c4785617b11c14715d7a109c65
tr1pplehf <danilyakimenko11@gmail.com>
2026-09-13 11:34:44 +0700

    #77 Anime-Vibe: Рефакторинг.

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = (props) => {
                     className={classNames('header__menu-link', {
                       'is-active': href === url
                     })}
-                    href={getUrl({href})}
+                    href={getUrl(href)}
                   >
                     {label}
                   </a>
